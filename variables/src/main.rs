71970d0df22ffc8693089d6448ed620df563be4c
wanglilong <wanglilong@croot.com>
2024-12-11 16:33:23 +0800

    gitignore

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -32,11 +32,14 @@ fn main() {
     let a = [3; 5]; // let a = [3,3,3,3,3]
     println!("{}",months[0]);
 
-    another_function(5,6);
+    let sum = another_function(5,6);
+    println!("{}",sum);
 }
 
-fn another_function(x: i32, y: i32) {
+fn another_function(x: i32, y: i32) -> i32 {
     println!("Another function.");
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+    // x + y
+    return x+y;
 }
